5289152a465fb60fef83fa7b3701b990f7b94eb0
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-08-27 17:45:11 +0300

    Theme preparation

---
+++ KVCGayrimenkulWebUI/ViewComponents/LayoutComponents/_LayoutSidebarPartialComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KVCGayrimenkulWebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutSidebarPartialComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke() 
+		{  return View(); }
+	}
+}
--- KVCGayrimenkulWebUI/obj/Debug/net6.0/KVCGayrimenkulWebUI.AssemblyInfo.cs
+++ KVCGayrimenkulWebUI/obj/Debug/net6.0/KVCGayrimenkulWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a0d4fcb3267c2f4590b60cc96125dc63a93cae")]
 [assembly: System.Reflection.AssemblyProductAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
