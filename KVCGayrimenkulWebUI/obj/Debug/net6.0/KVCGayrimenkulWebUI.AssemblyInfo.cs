b7e719b1d0ac97d18b448530e497838ce704faf2
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-09-16 16:17:14 +0300

    UI design

--- KVCGayrimenkulWebUI/obj/Debug/net6.0/KVCGayrimenkulWebUI.AssemblyInfo.cs
+++ KVCGayrimenkulWebUI/obj/Debug/net6.0/KVCGayrimenkulWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e5ec3a11557e87cd3f82faeb724c4d6aaac29b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12e1261dd42d6c57ef9e4e96ddd1097f5e7fe9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
