4428f2b24d079e143e718f2c6ddf89b98638c1d7
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-09-30 17:27:18 +0300

    Design update

--- KVCGayrimenkulWebUI/ViewComponents/DefaultComponents/_DefaultAdsComponentPartial.cs
+++ KVCGayrimenkulWebUI/ViewComponents/DefaultComponents/_DefaultAdsComponentPartial.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KVCGayrimenkulWebUI.Dtos.AdvertisementDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace KVCGayrimenkulWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultAdsComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultAdsComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
         }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7147/api/Advertisement/AdvertisementWithAdvertisementTypeAndCategory");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultAdvertisementDto>>(jsonData);
+            return View(values);
+        }
+    
     }
 }
--- KVCGayrimenkulWebUI/obj/Debug/net6.0/KVCGayrimenkulWebUI.AssemblyInfo.cs
+++ KVCGayrimenkulWebUI/obj/Debug/net6.0/KVCGayrimenkulWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12e1261dd42d6c57ef9e4e96ddd1097f5e7fe9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e719b1d0ac97d18b448530e497838ce704faf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KVCGayrimenkulWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
