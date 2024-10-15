92ae77f3e4c4596c3616532bc4fc3a3c73d5b8af
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-10-15 16:56:04 +0300

    satisfaction update

--- KVCGayrimenkulWebUI/ViewComponents/DefaultComponents/_DefaultSatisfactionComponentPartial.cs
+++ KVCGayrimenkulWebUI/ViewComponents/DefaultComponents/_DefaultSatisfactionComponentPartial.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using KVCGayrimenkulWebUI.Dtos.AdvertisementDtos;
 
 namespace KVCGayrimenkulWebUI.ViewComponents.DefaultComponents
 {
-    public class _DefaultSatisfactionComponentPartial:ViewComponent
+    public class _DefaultSatisfactionComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSatisfactionComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7147/api/Advertisement");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var advertisements = JsonConvert.DeserializeObject<List<ResultAdvertisementDto>>(jsonData);
+                var adCount = advertisements.Count;
+                ViewBag.AdCount = adCount;
+            }
+            else
+            {
+                ViewBag.AdCount = 0; // Hata durumunda 0 dönebiliriz
+            }
             return View();
         }
     }
